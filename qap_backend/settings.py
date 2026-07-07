637f62fe2e042b1dd658c17d837f0644abc2cce1
Your Name <your-email@example.com>
2026-07-07 23:36:07 +0100

    Add local IP address to CORS_ALLOWED_ORIGINS in settings

--- qap_backend/settings.py
+++ qap_backend/settings.py
@@ -184,6 +184,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://localhost:5173",
+    "http://192.168.227.193:5173",
     # "https://32b2-102-89-68-171.ngrok-free.app/",
 ]
 
